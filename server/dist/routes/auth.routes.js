c1eeef0f8a21d40dfcbea11dd4fee6c3b3917bd0
Jorge Betancur <jabetancur12@gmail.com>
2025-12-21 15:48:26 -0500

    chore: remove server/dist from git tracking

--- server/dist/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_controller_1 = require("../controllers/auth.controller");
-const router = (0, express_1.Router)();
-router.post('/login', auth_controller_1.login);
-router.get('/setup-status', auth_controller_1.setupStatus);
-router.post('/setup-admin', auth_controller_1.setupAdmin);
-exports.default = router;
