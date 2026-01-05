61e5cbac9ca5a8bf36d13086469c3cde6266e45d
Jorge Betancur <jabetancur12@gmail.com>
2026-01-05 13:33:15 -0500

    fix: use correct speed-insights adapter for vite/react

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -8,6 +9,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
       <App />
+      <SpeedInsights />
     </ThemeProvider>
   </StrictMode>,
 )
